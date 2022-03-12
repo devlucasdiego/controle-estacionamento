1889f065dd5921dcfa8aac5e6949886d6da49d3f
Lucas Diego <luucasdiego_12@hotmail.com>
2022-03-11 22:16:59 -0400

    Classe Service

--- src/main/java/com/api/controleestacionamento/services/VagaEstacionamentoService.java
+++ src/main/java/com/api/controleestacionamento/services/VagaEstacionamentoService.java
@@ -1,7 +1,15 @@
 package com.api.controleestacionamento.services;
 
+import com.api.controleestacionamento.repositories.VagaEstacionamentoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class VagaEstacionamentoService {
+
+    final VagaEstacionamentoRepository vagaEstacionamentoRepository;
+
+    public VagaEstacionamentoService(VagaEstacionamentoRepository vagaEstacionamentoRepository) {
+        this.vagaEstacionamentoRepository = vagaEstacionamentoRepository;
+    }
 }
