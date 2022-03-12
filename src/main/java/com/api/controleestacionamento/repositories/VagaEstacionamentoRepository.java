1ea956a08b615a05967cd5431647c832450aab56
Lucas Diego <luucasdiego_12@hotmail.com>
2022-03-11 21:48:30 -0400

    Classe Repository

---
+++ src/main/java/com/api/controleestacionamento/repositories/VagaEstacionamentoRepository.java
@@ -0,0 +1,12 @@
+package com.api.controleestacionamento.repositories;
+
+import com.api.controleestacionamento.models.VagaEstacionamentoModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface VagaEstacionamentoRepository extends JpaRepository<VagaEstacionamentoModel, UUID> {
+
+}
---
+++ src/main/java/com/api/controleestacionamento/services/VagaEstacionamentoService.java
@@ -0,0 +1,7 @@
+package com.api.controleestacionamento.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class VagaEstacionamentoService {
+}
